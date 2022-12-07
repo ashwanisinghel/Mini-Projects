ae013301a92be58232ff5fcc8143b346175a05be
ashwanisinghel <singh111ashwani@gmail.com>
2022-12-07 15:08:01 +0530

    axios.get

--- Appointment Scheduler/script.js
+++ Appointment Scheduler/script.js
@@ -14,18 +14,20 @@ function initListioners(){
     document.addEventListener('DOMContentLoaded',getItem)
 }
 function getItem(){
-    let details;
-    if(localStorage.getItem('details')===null){
-        details=[]
-    }else{
-       details = JSON.parse(localStorage.getItem('details'));
-    }
-
-    details.forEach((element)=>{
-        let li=document.createElement('li');
-        li.innerHTML=`${element.name}, ${element.email}, ${element.phone} <span><input type="submit" value="X" class="xbutton"></span>`;
-        listOfAppointments.appendChild(li);
-    })
+    // let details;
+    // if(localStorage.getItem('details')===null){
+    //     details=[]
+    // }else{
+    //    details = JSON.parse(localStorage.getItem('details'));
+    // }
+    axios.get('https://crudcrud.com/api/462e9c1b4b304856be4959e9566c2383/appointmentData').then((response)=>{
+        // console.log(response)
+        response.data.forEach((element)=>{
+            let li=document.createElement('li');
+            li.innerHTML=`${element.name}, ${element.email}, ${element.phone} <span><input type="submit" value="X" class="xbutton"></span>`;
+            listOfAppointments.appendChild(li);
+        })
+    }).catch((err)=>console.log(err));
 }
 
 function onAppointmentForm(e){
@@ -35,7 +37,7 @@ function onAppointmentForm(e){
         phone: phoneNumberIp.value
     }
     // saveFormDetailsToLocalStorage(details)
-    axios.post('https://crudcrud.com/api/462e9c1b4b304856be4959e9566c2383/appointmentData',details)
+    axios.post('https://crudcrud.com/api/462e9c1b4b304856be4959e9566c2383/appointmentData',details).then((response)=>console.log(response)).catch((err)=>console.log(err));
     
     let li=document.createElement('li');
     li.innerHTML=`${details.name}, ${details.email}, ${details.phone} <span><input type="submit" value="X" class="xbutton"></span>`;
