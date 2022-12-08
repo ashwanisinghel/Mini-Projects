e2bf34a69346fb13e0adcdd6d87658e2bb0bc1dc
ashwanisinghel <singh111ashwani@gmail.com>
2022-12-08 11:31:34 +0530

    axios.put_withError

--- Appointment Scheduler/script.js
+++ Appointment Scheduler/script.js
@@ -11,6 +11,8 @@ initListioners();
 function initListioners(){
     appointmentForm.addEventListener('submit',onAppointmentForm);
     listOfAppointments.addEventListener('click',removeListOfAppointments);
+    listOfAppointments.addEventListener('click',editAppointment);
+
     document.addEventListener('DOMContentLoaded',getItem)
 }
 function getItem(){
@@ -20,7 +22,7 @@ function getItem(){
     // }else{
     //    details = JSON.parse(localStorage.getItem('details'));
     // }
-    axios.get('https://crudcrud.com/api/462e9c1b4b304856be4959e9566c2383/appointmentData').then((response)=>{
+    axios.get('https://crudcrud.com/api/effb2d232a144a64b0f9c6c32eae66bf/appointmentData').then((response)=>{
         // console.log(response)
         response.data.forEach((element)=>{
             let li=document.createElement('li');
@@ -37,7 +39,7 @@ function onAppointmentForm(e){
         phone: phoneNumberIp.value
     }
     // saveFormDetailsToLocalStorage(details)
-    axios.post('https://crudcrud.com/api/462e9c1b4b304856be4959e9566c2383/appointmentData',details).then((response)=>console.log(response)).catch((err)=>console.log(err));
+    axios.post('https://crudcrud.com/api/effb2d232a144a64b0f9c6c32eae66bf/appointmentData',details).then((response)=>console.log(response)).catch((err)=>console.log(err));
     
     let li=document.createElement('li');
     li.innerHTML=`${details.name}, ${details.email}, ${details.phone} <span><input type="submit" value="X" class="xbutton"></span>`;
@@ -77,12 +79,37 @@ function removeAppointmentFromLocalStorage(content){
     // })
 
     // localStorage.setItem('details',JSON.stringify(details));
-    axios.get('https://crudcrud.com/api/462e9c1b4b304856be4959e9566c2383/appointmentData').then((response)=>{
+    axios.get('https://crudcrud.com/api/effb2d232a144a64b0f9c6c32eae66bf/appointmentData').then((response)=>{
         response.data.forEach((element)=>{
             if(content.includes(element.phone)){
                 // console.log(element._id);
-                axios.delete(`https://crudcrud.com/api/462e9c1b4b304856be4959e9566c2383/appointmentData/${element._id}`)
+                axios.delete(`https://crudcrud.com/api/effb2d232a144a64b0f9c6c32eae66bf/appointmentData/${element._id}`)
             }
         })
+    }).catch((err)=>{
+        console.log(err)
     })
+}
+
+function editAppointment(e){
+    if(!(e.target.classList.contains('xbutton'))){
+        // console.log(e.target.textContent)
+        axios.get('https://crudcrud.com/api/effb2d232a144a64b0f9c6c32eae66bf/appointmentData').then((response)=>{
+            response.data.forEach((element)=>{
+                if(e.target.textContent.includes(element.phone)){
+                    nameIp.value=element.name;
+                    emailIp.value=element.email;
+                    phoneNumberIp.value=element.phone;
+                }
+                appointmentForm.addEventListener('submit',()=>{
+                    const details={
+                        name: nameIp,
+                        email: emailIp,
+                        phone: phoneNumberIp,
+                    }
+                    axios.put(`https://crudcrud.com/api/effb2d232a144a64b0f9c6c32eae66bf/appointmentData/${element._id}`,details);
+                })
+            })
+        })
+    }
 }
