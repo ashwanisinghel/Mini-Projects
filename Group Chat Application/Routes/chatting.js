8bf5e670011a8423d9a33f525c487a075d891c62
ashwanisinghel <singh111ashwani@gmail.com>
2023-01-09 16:51:54 +0530

    login Handeled

--- Group Chat Application/Routes/chatting.js
+++ Group Chat Application/Routes/chatting.js
@@ -1,13 +1,16 @@
 const express=require('express');
+const fs=require('fs');
 
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.send('<form action="/chat" method="post"><input type="text" placeholder="message"><button type="submit">Send</button></form>')
+    const message=fs.readFileSync('message.txt');
+    res.send(`<h3>: ${message}</h3><form action="/chat" method="post"><input type="text" placeholder="message" name="message"><button type="submit">Send</button></form>`);
 })
 
 router.post('/',(req,res,next)=>{
+    fs.appendFileSync('message.txt',req.body.message);
     console.log(req.body);
-    res.redirect('/chat')
+    res.redirect('/chat');
 })
 module.exports=router;
--- Group Chat Application/Routes/login.js
+++ Group Chat Application/Routes/login.js
@@ -1,13 +1,16 @@
+const bodyParser = require('body-parser');
 const express=require('express');
+
 const router=express.Router();
 
+
 router.get('/',(req,res,next)=>{
-    res.send('<form action="/chat" method="post"><input type="text" placeholder="usename" name="urername"><button type="submit">Login</button></form>')
+    res.send(`<form action="/chat" onsubmit=localStorage.setItem('username',document.getElementById('username').value) method="post"><input type="text" placeholder="message" id='username' name="message"><button type="submit">Send</button></form>`)
 })
 
 router.post('/',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/chat')
+    console.log(req.body.username);
+    res.redirect('/chat');
 })
 
 module.exports=router;
